595e4abdc4d067f5d7632d70769c3811df871b1d
Entisar <Code.g.light@gmail.com>
2025-05-11 12:56:56 +0300

    ...

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let img = document.getElementById("coffeeImage");
 
-//the CORS proxy used to avoid CORS issues : https://cors.lol/
 
 setInterval(() => {
-    fetch("https://api.cors.lol/?url=coffee.alexflipnote.dev/random.json")
+    fetch("https://coffee.alexflipnote.dev/random.json")
         .then((response) => response.json())
         .then((data) => {
             console.log(data);
